7b123fc4625a2194896b3202c3838d4643bccff7
niranjan411 <niranjanmemane47@gmail.com>
2024-11-13 00:01:59 +0530

    5

--- menu.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("hamburger-toggle").addEventListener("change", function() {
-    const box = document.getElementById("mainLinks");
-    if (this.checked) {
-        box.style.display = "block";  // Show the box
-    } else {
-        box.style.display = "none";   // Hide the box
-    }
-});
